b627a9c5c7364040c68c86ef8c3653b2c88abeac
jerry8534 <jerryjjinu@gmail.com>
2023-01-06 17:42:51 +0900

    aa

---
+++ TravelMaker/src/main/java/com/project1/tourapi/testVo.java
@@ -0,0 +1,22 @@
+package com.project1.tourapi;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class testVo {
+	String contentID;
+	String title;
+	String firstImage;
+	String overView;
+	String addr1;
+	String addr2;
+	String tel;
+	String homepage;
+	String mapX;
+	String mapY;
+	String contentTypeId;
+	String cat1;
+	String firstImage2;
+}
---
+++ TravelMaker/src/main/resources/static/js/myTourSelect.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+ /* 후기작성 페이지 연결 */
+ $('.btnReview1').on('click',function(){
+	 $('#content').load('/myTour/reviewInsert');
+ })
+ 
